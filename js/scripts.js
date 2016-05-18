ca375bbaebc135add03bd74ef24341a5ecdcc4a9
Varvara Gayda <mamasetta@gmail.com>
2016-05-18 14:35:57 +0500

    карта

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,29 @@ window.addEventListener("keydown", function(event) {
     }
   }
 });
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938631, 30.323055],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../img/map.jpg',
+            // Размеры метки.
+            iconImageSize: [218, 142],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-3, -42]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
